1f7eb8c983120fd246b32b7b083e07f757af0f79
yurib@rssi.com <yurib@rssi.com>
2016-09-30 15:43:53 +0100

    Initial commit of BIM rooms with broken server

---
+++ backEnd/brServer.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+
+const port = 4000;
+const app = express();
+
+//var rooms = require('./rooms');
+//app.use('/rooms', rooms);
+
+var logger = require('./logger');
+app.use(logger);
+
+//app.use(express.static('../src'));
+
+app.listen(port, function(err){
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`http://localhost:${port}`);
+  }
+});
---
+++ backEnd/rooms.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var roomsRouter = express.Router();
+var path = require('path');
+
+var myRooms = [{'id':1,'number':'One'},{'id':2,'number':'Two'},{'id':3,'number':'Three'}];
+
+var rooms = myRooms;//JSON.parse(list_rooms.json);
+
+roomsRouter.get('/', function (request, response){
+    console.log("Hello world Dev!")
+    response.status(200).json(path.join(__dirname,'/listofrooms.json'));
+});
+
+roomsRouter.get('/getroom', function (request, response){
+  response.json(myRooms);
+});
+
+roomsRouter.get('/getroom/:id', function (request, response){
+  response.json(myRooms[request.params.id.id]);
+});
+
+module.exports = roomsRouter;
