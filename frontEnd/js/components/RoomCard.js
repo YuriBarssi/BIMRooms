68a5a1a133e65d3fb9fbbeb03453680a8473aa36
JasSidhu <jassidhu@ie.ibm.com>
2016-11-03 18:30:59 +0000

    my changes

--- frontEnd/js/components/RoomCard.js
+++ frontEnd/js/components/RoomCard.js
@@ -3,20 +3,35 @@ import ReactDom from 'react-dom';
 
 
 class RoomCard extends Component {
+
+  generateID() {
+    return '#' + this.props.id;
+  }
   render() {
     return (
       <div className="col m4">
         <div className="card">
          <div className="card-image">
-           <img src={this.props.cardImage} />
+           <img className="imgRow" src={this.props.cardImage} />
            <span className="card-title">{this.props.cardTitle}</span>
          </div>
          <div className="card-content">
            <p>{this.props.cardContent}</p>
          </div>
          <div className="card-action">
-           <a href="#">Click to open</a>
+           <a href = {this.generateID()} className="waves-effect waves-light btn modal-trigger">Click to open</a>
          </div>
+         {/* Below is the modal*/}
+         <div id={this.props.id} className="modal">
+          <div class="modal-content">
+            <img className="imgModal" src={this.props.cardImage} />
+            <h4>Modal Header</h4>
+            <p>A bunch of text</p>
+          </div>
+          <div class="modal-footer">
+            <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+          </div>
+        </div>
        </div>
       </div>
     );
--- frontEnd/js/init.js
+++ frontEnd/js/init.js
@@ -1,7 +1,16 @@
-(function($){
+/*(function($){
   $(function(){
 
     $('.button-collapse').sideNav();
-
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+*/
+$(document).ready(function(){
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+   $('.modal-trigger').leanModal();
+});
+function myModal(modalID) {
+  $('#modal1').openModal();
+}
